1bf1e36ae2237b694e3f3ba7e2af439777d4fd2b
samuellau0802 <samuellau0802@gmail.com>
2024-11-11 22:41:29 +0800

    init, map added

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Get started
+To install all the dependencies, please run `npm install`.
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import "./index.css";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+// url to a valid topojson file
+
+  const App = () => (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+
+      <ComposableMap>
+        <Geographies geography="/features.json">
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
     </div>
   );
-}
 
 export default App;
