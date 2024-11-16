12a6d6663627034829a97c2f679a740c0619e7d0
samuellau0802 <samuellau0802@gmail.com>
2024-11-16 21:07:20 +0800

    fix line chart bug, map projection

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -10,6 +10,7 @@ import {
     ResponsiveContainer
 } from 'recharts';
 import data from '../data/countries_data.json';
+import { Container } from '@mui/material';
 
 // Function to extract data for the specified country and indicators
 const getDataForCountry = (country, yearRange, indicator1, indicator2) => {
@@ -40,27 +41,35 @@ const getDataForCountry = (country, yearRange, indicator1, indicator2) => {
 };
 
 // LineChartComponent to visualize the data
+
+
 const LineChartComponent = ({ country, yearRange, indicator1, indicator2 }) => {
     const chartData = getDataForCountry(country, yearRange, indicator1, indicator2);
+
+    console.log(chartData);
 
     return (
         <div>
             <h3>{`Line Chart for ${country}: ${indicator1} and ${indicator2} (${yearRange[0]} - ${yearRange[1]})`}</h3>
-            {chartData.length > 0 ? (
-                <ResponsiveContainer width="100%" height={400}>
+            <div style={{height: "400px", width: "100%"}}>
+            {chartData.length > 0 ? ( 
+                 <ResponsiveContainer width="100%" height={400}>
                     <LineChart data={chartData}>
-                        <CartesianGrid strokeDasharray="3 3" />
+                        <CartesianGrid/>
                         <XAxis dataKey="year" />
-                        <YAxis />
+                        <YAxis yAxisId="left" />
+                        <YAxis yAxisId="right" orientation="right" />
                         <Tooltip />
                         <Legend />
-                        <Line type="monotone" dataKey={indicator1} stroke="#8884d8" />
-                        <Line type="monotone" dataKey={indicator2} stroke="#82ca9d" />
+                        <Line connectNulls yAxisId="left" type="monotone" dataKey={indicator1} stroke="#8884d8"  />
+                        <Line connectNulls yAxisId="right" type="monotone" dataKey={indicator2} stroke='#82ca9d' />
                     </LineChart>
+
                 </ResponsiveContainer>
             ) : (
                 <p>No data available for the specified range.</p>
             )}
+            </div>
         </div>
     );
 };
