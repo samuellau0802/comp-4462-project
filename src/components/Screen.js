a042bf6f5244cfba6ebe1a0a55ba21834ac62bf3
samuellau0802 <samuellau0802@gmail.com>
2024-11-20 19:22:30 +0800

    Refactor CSS files and remove unused code, updated dependencies

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import YearRangeSlider from "./YearRangeSlider";
 import '../App.css';
 import ChoroplethMap from "./ChoroplethMap";
